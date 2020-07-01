ba2b4a0b708cd70276e1faa68634d51ff203958f
Ammon Jeffery <savingphace@gmail.com>
2020-07-01 17:05:21 -0600

    testing ajax request

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const apiKey = "4d386df1a445943047c0faf9efafd3c0"
 
-var queryURL ="api.openweathermap.org/data/2.5/weather?q=" + {city name} + "&appid=" + apiKey
+var queryURL ="api.openweathermap.org/data/2.5/weather?q=" + "provo" + "&appid=" + apiKey
+$.ajax({
+    url = queryURL,
+    method = "GET",
+
+}).then(function (response) {
+    console.log(response)
+}
