d0409cb4809565dd74ab3626b48a3f9f3efb1e60
Ammon Jeffery <savingphace@gmail.com>
2020-07-01 20:51:43 -0600

    fixed errors with Ajax request.

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const apiKey = "4d386df1a445943047c0faf9efafd3c0"
+const apiKey = "4f7869ce3d5722411e808b80d43f12ba"
+let locationSafe = [];
 
-var queryURL ="api.openweathermap.org/data/2.5/weather?q=" + "provo" + "&appid=" + apiKey
-$.ajax({
-    url = queryURL,
-    method = "GET",
 
-}).then(function (response) {
-    console.log(response)
+
+
+function getCurrent (city) {
+    var queryURL ="http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey
+    $.ajax({
+        url : queryURL,
+        method : "GET"
+    
+    }).then(function (response) {
+        console.log(response)
+    })
 }
+
+
+$("#searchBtn").on("click", function () {
+    let cityName = $("#searchInput").val().trim();
+    getCurrent(cityName);
+})
